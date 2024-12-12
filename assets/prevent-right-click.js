59efdfd5b5a12c17afe5a1c24c03747e96c0c40b
alexerne-git <alexiserne00@gmail.com>
2024-12-12 23:50:32 +0100

    Deployed d2d0eeb with MkDocs version: 1.6.1

---
+++ assets/prevent-right-click.js
@@ -0,0 +1,5 @@
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();  // Disable the right-click context menu
+    alert("Right-click has been disabled on this site.");
+  });
+  
